0516e444c2cb0ddf8e9ba180697862d544ea23e9
dn440 <dadonakladal@gmail.com>
2020-05-07 19:45:52 +0100

    improve focus resolution

--- focus.sh
+++ focus.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-python /home/pi/cytosmarter/cytosmarter/LED-on.py
+python /home/pi/cytosmarter/LED-on.py
 # run with minimal settings
-camorama -m -d=/dev/video0
+# camorama -m -d=/dev/video0
+
+# run with specific settings
+# camorama -x 320 -y 240 -d=/dev/video0
+camorama -x 352 -y 288 -d=/dev/video0
